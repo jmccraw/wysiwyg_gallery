83252eb4e082687b4978d2c331b75d5e3a30206b
Justin McCraw <justin.mccraw@espn.com>
2020-08-10 20:19:08 -0400

    Add a helper button that clears the localStorage

---
+++ src/components/ClearHelper.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { clearStore } from '../utilities/LocalStore.js';
+  import { isToggled } from '../utilities/ToggleStore.js';
+
+  /**
+   * Clear the entire LocalStore on confirm
+   */
+  function clearLocalStore() {
+    const shouldClear = window.confirm( 'This erases all saved content and returns the page to ' +
+      'the default state and cannot be undone.\nAre you sure you want to do this?\n(OK = Yes, Cancel = No)' );
+
+    if ( shouldClear ) {
+      clearStore();
+      window.location.href = window.location.href;
+    }
+  }
+</script>
+
+<style type="text/scss">
+  @import '../styles/vars';
+
+  button {
+    display: block;
+    margin: 100px auto;
+  }
+</style>
+
+{#if ! $isToggled}<button type="button" on:click={clearLocalStore}>Reset All Content</button>{/if}
