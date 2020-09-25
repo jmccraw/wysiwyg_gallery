7f520b07bc5d66e2d13b74bdaf2a8dca9330576c
Justin McCraw <justin.mccraw@espn.com>
2020-09-25 17:32:16 -0400

    Need to fix logic so the cursor appears at the end of the copy-paste insertion point, if possible

--- src/utilities/StripClipboard.js
+++ src/utilities/StripClipboard.js
@@ -9,7 +9,14 @@ export function stripClipboard( event ) {
   event.preventDefault();
   const _target = event.target;
   const pastedText = ( event.originalEvent || event ).clipboardData.getData( 'text/plain' );
-  window.console.log( 'CLIPBOARD pasted text', event, _target, pastedText );
 
-  _target.innerHTML = pastedText;
+  // Get any selection text the user might have highlighted. We'll be sure to replace this
+  // with the clipboard contents, instead of overwriting the whole text input area
+  const selection = window.getSelection().toString();
+  const selectionSearch = new RegExp( selection, 'i' );
+
+  window.console.log( 'CLIPBOARD pasted text', selection, _target, pastedText );
+
+  // Change the innerHTML, replacing the selected text with the pasted text
+  _target.innerHTML = _target.innerHTML.replace( selectionSearch, pastedText );
 }
