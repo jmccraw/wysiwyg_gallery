26d4e301e09fd5e2f29ce518b00f392c487e4bfa
Justin McCraw <justin.mccraw@espn.com>
2020-09-28 13:42:31 -0400

    Make some updates to Strip Clipboard, but pivoting to use ChangeToPlainText.js instead

--- src/utilities/StripClipboard.js
+++ src/utilities/StripClipboard.js
@@ -14,9 +14,25 @@ export function stripClipboard( event ) {
   // with the clipboard contents, instead of overwriting the whole text input area
   const selection = window.getSelection().toString();
   const selectionSearch = new RegExp( selection, 'i' );
+  const savedRange = window.getSelection().getRangeAt( 0 );
 
-  window.console.log( 'CLIPBOARD pasted text', selection, _target, pastedText );
+  window.console.log( 'CLIPBOARD: target, selection, pasted text', _target, selection, pastedText );
 
   // Change the innerHTML, replacing the selected text with the pasted text
   _target.innerHTML = _target.innerHTML.replace( selectionSearch, pastedText );
+
+  // Set the caret to the end of the selection, if available
+  // Modified from https://stackoverflow.com/a/4238971/1472477
+  // and https://stackoverflow.com/a/3323835/1472477
+  if ( 0 < selection.length ) {
+    // _target.focus();
+    const range = document.createRange();
+    // range.selectNodeContents( _target );
+    range.setStart( _target, startOffset );
+    range.setEnd( _target, startOffset + pastedText.length - 1 );
+    range.collapse( true );
+    // const sel = window.getSelection();
+    // sel.removeAllRanges();
+    // sel.addRange( range );
+  }
 }
