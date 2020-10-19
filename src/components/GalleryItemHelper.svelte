a112c315016cb9c0d132430b55140d2a018d74ea
Justin McCraw <justin.mccraw@espn.com>
2020-10-19 16:47:36 -0400

    Add a header to the WYSIWYG components of the Gallery Item and Gallery Slideshow so people know what they'll be adding

--- src/components/GalleryItemHelper.svelte
+++ src/components/GalleryItemHelper.svelte
@@ -8,6 +8,12 @@
     margin-top: 20px;
   }
 
+  .subhead.small {
+    font-size: 30px;
+    letter-spacing: 1px;
+    margin-bottom: -11px;
+  }
+
   .add-button {
     background-color: none;
     border-radius: 50%;
@@ -15,6 +21,7 @@
 </style>
 
 <aside>
+  <h2 class="subhead small">Add New Single Image</h2>
   <slot name="new-gallery-item" class="gallery-item"></slot>
   <slot name="add-new-gallery-item-button" class="add-button"></slot>
 </aside>
--- src/components/GallerySlideshowHelper.svelte
+++ src/components/GallerySlideshowHelper.svelte
@@ -9,6 +9,12 @@
     margin-top: 20px;
   }
 
+  .subhead.small {
+    font-size: 30px;
+    letter-spacing: 1px;
+    margin-bottom: 12px;
+  }
+
   .add-button {
     background-color: none;
     border-radius: 50%;
@@ -16,6 +22,7 @@
 </style>
 
 <aside>
+  <h2 class="subhead small">Add New Slideshow Images</h2>
   <slot name="new-gallery-slideshow-item" class="gallery-item"></slot>
   <slot name="add-new-gallery-slideshow-full-width-container"></slot>
   <slot name="add-new-gallery-slideshow-item-button" class="add-button"></slot>
